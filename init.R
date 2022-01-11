371c17a01b0a624cb47d2603b527e0cd14d57c43
V Maheysh <61049979+Zualemo-xo@users.noreply.github.com>
2022-01-12 00:19:27 +0530

    Update init.R

--- init.R
+++ init.R
@@ -3,7 +3,8 @@
 # R code to install packages if not already installed
 #
 
-my_packages = c("plotly", "DT","shiny","tidyverse","caret","dplyr","ggplot2","plotly","treemapify","stringr","ggthemes","plotrix")
+
+my_packages = c("plotly", "DT","shiny","tidyverse","caret","dplyr","ggplot2","plotly","treemapify","stringr","ggthemes","plotrix","shinythemes","data.table","shinydashboard")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
